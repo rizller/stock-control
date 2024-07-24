d94976a501803f2a3e68ef698c36c2238fff1a50
Rizller Souza <rizller.souza@prodemge.gov.br>
2024-07-24 19:34:45 -0300

    Created reactive login and signup forms

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,25 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   loginCard = true;
 
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  signupForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  constructor(private formBuilder: FormBuilder){ }
+
+  onSubmitLoginForm(): void {
+    console.log ("Dados do Formulário de Login: ", this.loginForm.value)
+  }
+
+  onSubmitSingupForm(): void{
+    console.log ("Dados do Formulário Signup: ", this.signupForm.value)
+  }
 
 }
